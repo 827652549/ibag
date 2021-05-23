f1d33b6f826e58e615b39674513c2d016657a315
macsu <827652549@qq.com>
2021-05-23 20:31:11 +0800

    fix

--- serve/start.js
+++ serve/start.js
@@ -18,22 +18,12 @@ const router = new Router();
 
 app.use(bodyParser());
 //解决跨域
-// app.use(cors());
-
-app.all("*",function(req,res,next){
-  //设置允许跨域的域名，*代表允许任意域名跨域
-  res.header("Access-Control-Allow-Origin","*");
-  //允许的header类型
-  res.header("Access-Control-Allow-Headers","content-type");
-  //跨域允许的请求方式
-  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-  if (req.method.toLowerCase() == 'options')
-    res.send(200);  //让options尝试请求快速结束
-  else
-    next();
-})
+app.use(cors());
 
 router.post("/start", (ctx, next) => {
+  ctx.set("Access-Control-Allow-Origin", "http://localhost:2078")
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
   console.log("ibag running……");
   resetOutput(ctx.request.body);
   ctx.body = "Building Your Project";
