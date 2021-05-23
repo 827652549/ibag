328dab576dd53305d0e3ead193df46e02fa71661
macsu <827652549@qq.com>
2021-05-23 13:43:28 +0800

    fix

--- web/ibag-guide-web/.cache/sync-requires.js
+++ web/ibag-guide-web/.cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/junking/WebstormProjects/ibag-temp/web/ibag-guide-web/.cache/dev-404-page.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/junking/WebstormProjects/ibag-temp/web/ibag-guide-web/src/pages/index.js")),
   "component---src-pages-step-1-js": preferDefault(require("/Users/junking/WebstormProjects/ibag-temp/web/ibag-guide-web/src/pages/step1.js")),
   "component---src-pages-step-2-js": preferDefault(require("/Users/junking/WebstormProjects/ibag-temp/web/ibag-guide-web/src/pages/step2.js")),
