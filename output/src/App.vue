bf2d1858528e0515ce5bcf1f3c47273ff55ece1e
macsu <827652549@qq.com>
2021-05-23 13:25:11 +0800

    fix

---
+++ output/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    Welcome to <md-avatar class="md-avatar-icon md-large md-accent">
+    <md-ripple>ibag</md-ripple>
+  </md-avatar>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 200px;
+  color: rgb(90, 88, 88);
+  font-size: 40px;
+}
+</style>
