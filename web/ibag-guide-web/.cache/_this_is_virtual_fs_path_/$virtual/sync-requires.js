958d0a985ce99b1dd68638b6008f30bb8ecfb16a
macsu <827652549@qq.com>
2021-05-23 13:09:40 +0800

    fix

---
+++ web/ibag-guide-web/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,14 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/junking/WebstormProjects/ibag-temp/web/ibag-guide-web/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/junking/WebstormProjects/ibag-temp/web/ibag-guide-web/src/pages/index.js")),
+  "component---src-pages-step-1-js": preferDefault(require("/Users/junking/WebstormProjects/ibag-temp/web/ibag-guide-web/src/pages/step1.js")),
+  "component---src-pages-step-2-js": preferDefault(require("/Users/junking/WebstormProjects/ibag-temp/web/ibag-guide-web/src/pages/step2.js")),
+  "component---src-pages-step-3-js": preferDefault(require("/Users/junking/WebstormProjects/ibag-temp/web/ibag-guide-web/src/pages/step3.js")),
+  "component---src-pages-step-4-js": preferDefault(require("/Users/junking/WebstormProjects/ibag-temp/web/ibag-guide-web/src/pages/step4.js"))
+}
+
---
+++ web/ibag-guide-web/.cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-step-1-js": () => import("./../../../src/pages/step1.js" /* webpackChunkName: "component---src-pages-step-1-js" */),
+  "component---src-pages-step-2-js": () => import("./../../../src/pages/step2.js" /* webpackChunkName: "component---src-pages-step-2-js" */),
+  "component---src-pages-step-3-js": () => import("./../../../src/pages/step3.js" /* webpackChunkName: "component---src-pages-step-3-js" */),
+  "component---src-pages-step-4-js": () => import("./../../../src/pages/step4.js" /* webpackChunkName: "component---src-pages-step-4-js" */)
+}
+
